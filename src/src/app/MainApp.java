8dfbf8c48a4f12df7502e0acdc6babd29e4fafa5
auxencecolson <auxence.colson@efrei.net>
2026-09-15 12:19:59 -0400

    test

--- src/src/app/MainApp.java
+++ src/src/app/MainApp.java
@@ -1,7 +1,10 @@
 package app;
+import model.*;
+
 
 public class MainApp {
-    static void main(String[] args) {
-        System.out.println("Fini");
+    public static void main(String[] args) {
+        Truck truck = new Truck(1, "Volvo FH", 120000, "Available", 150.0, "Diesel", 500, 2, 4.0, 8000.0, 20000, true);
+        System.out.println(truck.toString());
     }
 }
