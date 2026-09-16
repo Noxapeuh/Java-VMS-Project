f4d4552b213534c850dce95d8346c971d33ff323
auxencecolson <auxence.colson@efrei.net>
2026-09-16 09:57:00 -0400

    test

--- src/src/app/MainApp.java
+++ src/src/app/MainApp.java
@@ -6,5 +6,6 @@ public class MainApp {
     public static void main(String[] args) {
         Truck truck = new Truck(1, "Volvo FH", 120000, "Available", 150.0, "Diesel", 500, 2, 4.0, 8000.0, 20000, true);
         System.out.println(truck.toString());
+        System.out.println("Is the truck rentable? " + truck.isRentable());
     }
 }
--- src/src/model/Car.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public abstract class Car {
-    private String fuel;
-    private int horsePower;
-    private int passengersCapacity;
-}
